d44addd872eca23ab22eadf876a9ae8b09bcbc1e
noemiortona03 <noemi.ortona@epfl.ch>
2025-12-01 10:31:11 +0100

    noemi_update

--- src/data/filter_sports.py
+++ src/data/filter_sports.py
@@ -38,7 +38,7 @@ def find_match(subreddit, country_map):
         
     return None
 
-def filter_sports(countries_dataset, country_matches_list, unique_sub_list, final_folder):
+def filter_sports(df_post_with_1_country, country_matches_list, final_folder):
     # --- 1. Define the Sport Map (Alias -> Standard Category) ---
     # This map uses the same logic as your 'country_map'
     sport_map = {
@@ -102,7 +102,7 @@ def filter_sports(countries_dataset, country_matches_list, unique_sub_list, fina
         'starcraft': 'eSports', 'rocketleague': 'eSports'
     }
     df_matches_countries = pd.read_csv(country_matches_list)
-    matches = df_matches_countries.set_index('Subreddit')['Country'].to_dict()
+    matches = df_matches_countries.set_index('subreddit')['country'].to_dict()
     # --- 2. Loop to Find Sport Matches ---
 
     # Get the list of subreddits ALREADY identified as countries, to exclude them
@@ -110,9 +110,8 @@ def filter_sports(countries_dataset, country_matches_list, unique_sub_list, fina
 
     # Dictionary for the new sport matches
     sport_matches = {}
-
-    unique_sub_df = pd.read_csv(unique_sub_list, header=None)
-    unique_subreddit_list = unique_sub_df.iloc[:, 0].tolist()
+    all_subreddits_series = pd.concat([df_post_with_1_country['SOURCE_SUBREDDIT'], df_post_with_1_country['TARGET_SUBREDDIT']])
+    unique_subreddit_list = all_subreddits_series.unique().tolist()
 
     for s in tqdm(unique_subreddit_list):
         # SKIP if the subreddit has ALREADY been identified as a country
@@ -125,42 +124,8 @@ def filter_sports(countries_dataset, country_matches_list, unique_sub_list, fina
         if sport_category:
             sport_matches[s] = sport_category # Ex: {'r/formula1racing': 'Formula 1'}
 
-    df_country=pd.read_csv(countries_dataset)
-    df_country["SPORT_SOURCE"] = df_country["SOURCE_SUBREDDIT"].map(sport_matches)
-    df_country["SPORT_TARGET"] = df_country["TARGET_SUBREDDIT"].map(sport_matches)
+    df_sport_matches = pd.DataFrame(list(sport_matches.items()), columns=['subreddit', 'sport'])
+    df_sport_matches.to_csv(final_folder + "df_country_sport_map.csv", index=False)
+    df_sport_matches.to_csv(final_folder+"df_country_sport_map.csv", index=False)
 
-    new_order_s = ["SOURCE_SUBREDDIT","SOURCE_COUNTRY", "SPORT_SOURCE", "TARGET_SUBREDDIT", "TARGET_COUNTRY", "SPORT_TARGET", "POST_ID", "TIMESTAMP", "LINK_SENTIMENT", 
-        "num_chars", "num_chars_no_space", "frac_alpha", "frac_digits",
-        "frac_upper", "frac_spaces", "frac_special", "num_words",
-        "num_unique_words", "num_long_words", "avg_word_length",
-        "num_unique_stopwords", "frac_stopwords", "num_sentences",
-        "num_long_sentences", "avg_chars_per_sentence", "avg_words_per_sentence",
-        "readability_index", "sent_pos", "sent_neg", "sent_compound",
-        "LIWC_Funct", "LIWC_Pronoun", "LIWC_Ppron", "LIWC_I", "LIWC_We",
-        "LIWC_You", "LIWC_SheHe", "LIWC_They", "LIWC_Ipron", "LIWC_Article",
-        "LIWC_Verbs", "LIWC_AuxVb", "LIWC_Past", "LIWC_Present", "LIWC_Future",
-        "LIWC_Adverbs", "LIWC_Prep", "LIWC_Conj", "LIWC_Negate", "LIWC_Quant",
-        "LIWC_Numbers", "LIWC_Swear", "LIWC_Social", "LIWC_Family",
-        "LIWC_Friends", "LIWC_Humans", "LIWC_Affect", "LIWC_Posemo",
-        "LIWC_Negemo", "LIWC_Anx", "LIWC_Anger", "LIWC_Sad", "LIWC_CogMech",
-        "LIWC_Insight", "LIWC_Cause", "LIWC_Discrep", "LIWC_Tentat",
-        "LIWC_Certain", "LIWC_Inhib", "LIWC_Incl", "LIWC_Excl", "LIWC_Percept",
-        "LIWC_See", "LIWC_Hear", "LIWC_Feel", "LIWC_Bio", "LIWC_Body",
-        "LIWC_Health", "LIWC_Sexual", "LIWC_Ingest", "LIWC_Relativ",
-        "LIWC_Motion", "LIWC_Space", "LIWC_Time", "LIWC_Work", "LIWC_Achiev",
-        "LIWC_Leisure", "LIWC_Home", "LIWC_Money", "LIWC_Relig", "LIWC_Death",
-        "LIWC_Assent", "LIWC_Dissent", "LIWC_Nonflu", "LIWC_Filler"]
-
-    df_country = df_country[new_order_s]
-    # 1. Define your two conditions
-
-    # Condition 1: (SPORT_SOURCE is not NaN) AND (TARGET_COUNTRY is not NaN)
-    cond_1 = (df_country['SPORT_SOURCE'].notna()) & (df_country['TARGET_COUNTRY'].notna())
-
-    # Condition 2: (SPORT_TARGET is not NaN) AND (SOURCE_COUNTRY is not NaN)
-    cond_2 = (df_country['SPORT_TARGET'].notna()) & (df_country['SOURCE_COUNTRY'].notna())
-
-    # 2. Apply the filter
-    # Keep rows that satisfy Condition 1 OR Condition 2
-    df_country_sport = df_country[cond_1 | cond_2].copy()
-    df_country_sport.to_csv(final_folder+"df_country_sport.csv", index=False)
+    return sport_matches
